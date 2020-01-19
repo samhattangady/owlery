d2068aa8bca987d49b16154532bb537fc445bc7c
Samarth Hattangady <samhattangady@gmail.com>
2020-01-19 15:58:28 +0000

    feat: update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -2,5 +2,5 @@ import Config
 
 config :owlery,
   crossword_data_directory: "/home/samhattangady/current/",
-  crossword_data_endpoint: "https://storage.cloud.google.com/cluegrid-chapliboy/data/"
+  crossword_data_endpoint: "https://storage.googleapis.com/cluegrid-chapliboy/data/"
 
